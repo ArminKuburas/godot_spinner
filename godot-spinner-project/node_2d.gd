76bf700f446e8f461729098654c7a32e7badf00b
Armin Kuburas <49181652+ArminKuburas@users.noreply.github.com>
2024-09-18 06:45:27 +0300

    feat: added the ability to add in options where you add text and chance

--- godot-spinner-project/node_2d.gd
+++ godot-spinner-project/node_2d.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
-var options = ["Option 1", "Option 2", "Option 3", "Option 4"]
+var options = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize() # Replace with function body.
 	$Button.connect("pressed", Callable(self, "_on_button_pressed"))
+	$ConfirmButton.connect("pressed", Callable(self, "_on_confirm_button_pressed"))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,9 +14,30 @@ func _process(delta: float) -> void:
 	pass
 	
 func spin_wheel():
-	var selected_option = options[randi() % options.size()]
-	print ("Selected option ", selected_option)
+	if options.size() == 0:
+		print("No options available.")
+		return
+	var total_chance = 0
+	for option in options:
+		total_chance += option["chance"]
+	var random_value = randf() * total_chance
+	
+	var cumulative_chance = 0
+	for option in options:
+		cumulative_chance += option["chance"]
+		if random_value <= cumulative_chance:
+			print("Selected option: ", option["name"])
+			return
 
 
 func _on_button_pressed() -> void:
 	spin_wheel() # Replace with function body.
+
+
+func _on_confirm_button_pressed() -> void:
+	var option_name = $option_name.text
+	var chance = $option_chance.value
+	
+	options.append({"name": option_name, "chance": chance})
+	$option_chance.value = 0
+	$option_name.text = ""
