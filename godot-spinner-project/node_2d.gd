096c6ae6e2ec241d31e7f7c87a399b908ed7b449
Armin Kuburas <49181652+ArminKuburas@users.noreply.github.com>
2024-09-18 09:05:23 +0300

    feat: Added the ability to load and save created wheels

--- godot-spinner-project/node_2d.gd
+++ godot-spinner-project/node_2d.gd
@@ -1,12 +1,18 @@
 extends Node2D
 
 var options = []
+var save_path = "user://wheels/"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	randomize() # Replace with function body.
+	randomize()
 	$Button.connect("pressed", Callable(self, "_on_button_pressed"))
 	$ConfirmButton.connect("pressed", Callable(self, "_on_confirm_button_pressed"))
+	$wheel_save_button.connect("pressed", Callable(self, "_on_wheel_save_button_pressed"))
+	var dir = DirAccess.open("user://wheels")
+	if dir == null:
+		DirAccess.make_dir_absolute("user://wheels")
+	load_saved_wheels()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,7 +31,7 @@ func spin_wheel():
 	var cumulative_chance = 0
 	for option in options:
 		cumulative_chance += option["chance"]
-		if random_value <= cumulative_chance:
+		if random_value <= cumulative_chance && option["chance"] != 0:
 			print("Selected option: ", option["name"])
 			return
 
@@ -33,11 +39,47 @@ func spin_wheel():
 func _on_button_pressed() -> void:
 	spin_wheel() # Replace with function body.
 
+func load_saved_wheels() -> void:
+	var dir = DirAccess.open(save_path)
+	if dir != null:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if !dir.current_is_dir() and file_name.ends_with(".json"):
+				print("Saved wheel: ", file_name)
+				load_wheel(file_name)
+			file_name = dir.get_next()
+		dir.list_dir_end()
+
+func load_wheel(file_name: String) -> void:
+	var file = FileAccess.open(save_path + file_name, FileAccess.READ)
+	if file != null:
+		var data = JSON.parse_string(file.get_as_text())
+		file.close()
+		options = data
+		print("Loaded wheel: ", options)
 
 func _on_confirm_button_pressed() -> void:
 	var option_name = $option_name.text
 	var chance = $option_chance.value
-	
+	if $option_name.text == "":
+		print("Given input incorrect as a name is missing")
+		return
 	options.append({"name": option_name, "chance": chance})
 	$option_chance.value = 0
 	$option_name.text = ""
+
+func save_wheel(file_name: String) -> void:
+	var file = FileAccess.open(save_path + file_name + ".json", FileAccess.WRITE)
+	if file != null:
+		file.store_string(JSON.stringify(options))
+		file.close()
+
+
+func _on_wheel_save_button_pressed() -> void:
+	var wheel_name = $wheel_name.text
+	if wheel_name != "":
+		save_wheel(wheel_name)
+		$wheel_name.text = ""
+	else:
+		print("please provide a wheel name")
